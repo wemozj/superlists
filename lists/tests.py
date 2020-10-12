cfbcc7cc2324418e9835fe51a4b6112b07e26f12
wemozj <1348182282@qq.com>
2020-10-12 15:57:36 +0800

    Basic view now returns minimal HTML

--- lists/tests.py
+++ lists/tests.py
@@ -1,3 +1,4 @@
+from django.http import HttpRequest
 from django.test import TestCase
 
 # Create your tests here.
@@ -10,3 +11,11 @@ class HomePageTest(TestCase):
     def test_root_url_resolves_to_home_page_view(self):
         found = resolve('/') # 检查网站根路径‘/'，能否找到名为home_page的函数
         self.assertEqual(found.func, home_page)
+
+    def test_home_page_returns_correct_html(self):
+        request = HttpRequest() # 创建一个HTTPRequest对象
+        response = home_page(request) # 传给home_page视图，得到响应
+        html = response.content.decode('utf8') # 提取content，得到原始字节
+        self.assertTrue(html.startswith('<html>'))
+        self.assertIn('<title>To-Do lists</title>', html)
+        self.assertTrue(html.endswith('</html>'))
--- lists/views.py
+++ lists/views.py
@@ -1,5 +1,6 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
 def home_page(request):
-    pass
+    return HttpResponse("<html><title>To-Do lists</title></html>")

4fd004a63778e1e224437a83ca831a6468ad4867
wemozj <1348182282@qq.com>
2020-10-12 16:42:51 +0800

    Functional test now checks we can input a to-do item

--- functional_tests.py
+++ functional_tests.py
@@ -1,4 +1,5 @@
 # author: wemo  time:2020/10/12
+import time
 import unittest
 from selenium import webdriver
 
@@ -16,15 +17,31 @@ class NewVisitorTest(unittest.TestCase):
         self.browser.get('http://localhost:8000')
         # 她注意到网页的标题和头部都包含“TO-DO"这个词
         self.assertIn('To-Do', self.browser.title)
-        self.fail('Finish the test!')
+        header_text = self.browser.find_element_by_tag_name('h1').text
+        self.assertIn('To-Do', header_text)
         # 应用邀请她输入一个待办事项
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertEqual(
+            inputbox.get_attribute('placeholder'),
+            'Enter a to-do item'
+        )
         # 她在一个文本框中输入了“Buy peacock feathers"(购买孔雀羽毛）
         # wemo的爱好是使用假蝇做鱼饵钓鱼
+        inputbox.send_keys('Buy peacock feathers')
         # 她按下回车键后，页面更新了
         # 待办事项表格中显示了“1. Buy peacock feathers"
+        inputbox.send_keys(Keys.ENTER)
+        time.sleep(1)
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertTrue(
+            any(row.text == '1: Buy peacock feathers' for row in rows)
+        )
+
         # 页面中又显示了一个文本框，可以输入其他的待办事项
         # 她输入了“Use peacock feathers to make a fly"
         # wemo做事很有条理
+        self.fail(msg='Finish the test!')
         # 页面再次更新，她的清单中显示了这两个待办事项
         # wemo想知道这个网站是否会记住她的清单
         # 她看到网站为她生成了一个唯一的URL

b299234a06f4fce097bf2861926ee4d98b0efc96
wemozj <1348182282@qq.com>
2020-10-16 11:11:47 +0800

    refactor test code

--- functional_tests.py
+++ functional_tests.py
@@ -13,6 +13,11 @@ class NewVisitorTest(unittest.TestCase):
     def tearDown(self):
         self.browser.quit()
 
+    def check_for_row_in_list_table(self, row_text):
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn(row_text, [row.text for row in rows])
+
     def test_can_start_a_list_and_retrieve_it_later(self):
         # wemo听说有一个很酷的在线待办事项应用
         # 她去看了这个应用的首页
@@ -33,17 +38,19 @@ class NewVisitorTest(unittest.TestCase):
         # 她按下回车键后，页面更新了
         # 待办事项表格中显示了“1. Buy peacock feathers"
         inputbox.send_keys(Keys.ENTER)
-        time.sleep(1)
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
-        self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
+        time.sleep(2)
+        self.check_for_row_in_list_table('Buy peacock feathers')
 
         # 页面中又显示了一个文本框，可以输入其他的待办事项
         # 她输入了“Use peacock feathers to make a fly"
-        self.assertIn('2: Use peacock feathers to make a fly',
-                        [row.text for row in rows])
         # wemo做事很有条理
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys('Use peacock feathers to make a fly')
+        inputbox.send_keys(Keys.ENTER)
+        time.sleep(2)
         # 页面再次更新，她的清单中显示了这两个待办事项
+        self.check_for_row_in_list_table('1: Buy peacock feathers')
+        self.check_for_row_in_list_table('2: Use peacock feathers to make a fly')
         # wemo想知道这个网站是否会记住她的清单
         # 她看到网站为她生成了一个唯一的URL
         # 而且页面中有一些文字解说这个功能

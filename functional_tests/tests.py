9dd0aed6f1a7876593257e9dc826e6bb239ba63b
wemozj <1348182282@qq.com>
2020-10-20 10:43:07 +0800

    first steps of Function Test for layout + styling

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -102,4 +102,26 @@ class NewVisitorTest(LiveServerTestCase):
         self.assertIn('Buy milk', page_text)
         # 两人都很满意，然后睡觉去了
 
+    def test_layout_and_styling(self):
+        # 访问首页
+        self.browser.get(self.live_server_url)
+        self.browser.set_window_size(1024, 768)
+        # 看到输入框完美地居中显示
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertAlmostEqual(
+            inputbox.location['x'] + inputbox.size['width'] / 2,
+            512,
+            delta=10
+        )
 
+        # 新建了一个清单，看到输入框仍完美地居中显示
+        inputbox.send_keys('testing')
+        inputbox.send_keys(Keys.ENTER)
+        self.wait_for_row_in_list_table('1: testing')
+        # 看到输入框完美地居中显示
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertAlmostEqual(
+            inputbox.location['x'] + inputbox.size['width'] / 2,
+            512,
+            delta=10
+        )

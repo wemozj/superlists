31cd4d96b58265b0e935b6aff2fa53f1ad3b3946
wemozj <1348182282@qq.com>
2020-10-22 22:04:28 +0800

    user : ubuntu

--- superlists/settings.py
+++ superlists/settings.py
@@ -88,7 +88,7 @@ DATABASES = {
         'ENGINE' : 'django.db.backends.mysql',
         'NAME'   : 'lists_db',
         'USER'   : 'ubuntu',
-        'PASSWORD' : '1995zj',
+        'PASSWORD' : '',
         'HOST'   : 'localhost',
         'PORT'   : '3306',
     },
